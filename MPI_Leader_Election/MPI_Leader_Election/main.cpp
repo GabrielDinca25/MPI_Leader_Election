b5d0b7ef59ce864977486f47835ae838767d0063
Gabi\Gabriel <gdincagabriel@gmail.com>
2018-04-14 18:21:20 +0300

    Added  P2P implementation, application crashes

--- MPI_Leader_Election/MPI_Leader_Election/main.cpp
+++ MPI_Leader_Election/MPI_Leader_Election/main.cpp
@@ -8,7 +8,10 @@
 int main(int argc, char** argv) {
 
 	int numprocs, procid, len, partner, message;
-	int value = 0, max = 0, recvvalue = 0;
+    int value = 0;
+    int value_id_pair[2] = { 0, 0 };
+    int recv_value_id_pair[2] = { 0, 0 };
+    int max_value_id_pair[2] = { 0, 0 };
 
 	char hostname[MPI_MAX_PROCESSOR_NAME];
 
@@ -21,18 +24,42 @@ int main(int argc, char** argv) {
 
 	srand(time(NULL));
 	value = rand() % numprocs;
+    printf("Process %d generated number: %d \n", procid, value);
+    value_id_pair[0] = value;
+    value_id_pair[1] = procid;
+    for (int i = 0; i < numprocs; i++)
+    {
+        if (i != procid)
+        {
+            MPI_Send(&value_id_pair, 2, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Recv(&recv_value_id_pair, 2, MPI_INT, i, procid, MPI_COMM_WORLD, &status);
+            if (max_value_id_pair[0] < recv_value_id_pair[0])
+            {
+                max_value_id_pair[0] = recv_value_id_pair[0];
+                max_value_id_pair[1] = recv_value_id_pair[1];
+            }
+            else if (max_value_id_pair[0] == recv_value_id_pair[0])
+            {
+                if (max_value_id_pair[1] < recv_value_id_pair[1])
+                {
+                    max_value_id_pair[0] = recv_value_id_pair[0];
+                    max_value_id_pair[1] = recv_value_id_pair[1];
+                }
+            }
+        }
+    }
 
-	if (procid == MASTER)
-	{
-		MPI_Send(&value, 1, MPI_INT, procid + 1, 1, MPI_COMM_WORLD);
-	}
-	else
-	{
+    for (int i = 0; i < numprocs;i++)
+    {
+        if (i != procid)
+        {
+           
+        }
+    }
 
-	}
 
-	MPI_Send(&subvector, step, MPI_INT, i, i, MPI_COMM_WORLD);
-	MPI_Recv(&subvector, step, MPI_INT, MASTER, procid, MPI_COMM_WORLD, &status);
+    MPI_Finalize();
+    printf("Leader is process %d with the value: %d", max_value_id_pair[1], max_value_id_pair[0]);
 
 	
 
